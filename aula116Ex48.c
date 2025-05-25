e8cf1e92d6106b2219487b3b3c2dc8044ec2d3d6
davyusow <dayvsonf27@gmail.com>
2025-05-24 21:34:33 -0300

    Exercício 48
    Faça um programa que leia 10 números reais e os armazene em um vetor. Em seguida, leia um
    código inteiro e faça uma das ações abaixo:
    0 – finaliza o programa;
    1 – imprime o vetor na ordem do início ao fim;
    2 – imprime o vetor na ordem inversa (do fim para o início).
    O programa deve funcionar até que o usuário digite 0 para finalizar.

--- aula116Ex48.c
+++ aula116Ex48.c
@@ -11,25 +11,48 @@ int lerInteiro(char *mensagem) {
   return valorLido;
 }
 
-int* insertionSort(int* vetor,int tam){
-    for(int i = 0;i<tam;i++){
-        if()
-
-    }
+float lerReal(char *mensagem) {
+  float valorLido;
+  printf(" %s", mensagem);
+  while (scanf(" %f", &valorLido) != 1) {
+    while (getchar() != '\n');
+    printf(" %s", mensagem);
+  }
+  return valorLido;
 }
 
 int main(void){
-    int valores[10];
+    float valores[10];
     int opcao;
     for(int contador = 0;contador<10;contador++){
         printf("%2i - ",contador+1);
-        valores[contador] = lerInteiro("Insira um valor no vetor: ");
+        valores[contador] = lerReal("Insira um valor no vetor: ");
     }
     do{
-        system("clear");
-        printf("1 - Listar o vetor ordenado\n2 - Listar o vetor na ordem inversa\n0 - sair\n");
+        printf("\n1 - Listar o vetor inicio ao fim\n2 - Listar o vetor na ordem inversa\n0 - sair\n");
         opcao = lerInteiro("Opção: ");
+        switch(opcao){
+            default:
+                printf("Valor inválido!\n");
+                break;
+            case 1:
+                for(int contador = 0;contador < 10;contador++){
+                    printf("%.2f ",valores[contador]);
+                }
+                printf("\n");
+                break;
+            case 2:
+            for(int contador = 9;contador > -1;contador--){
+                    printf("%.2f ",valores[contador]);
+                }
+                printf("\n");
+                break;
+            case 0:
+                printf("\nAté logo!\n");
+                break;
+        }
     }while(opcao!=0);
+    
 
     return 0;
 }
