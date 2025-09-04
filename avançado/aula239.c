8e1f43e3c6d8c7629f73c8a3b85ee4c8a62d5b7e
davyusow <dayvsonf27@gmail.com>
2025-09-03 21:45:10 -0300

    Adição dos arquivos da aula 239

---
+++ avançado/aula239.c
@@ -0,0 +1,108 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no {
+  int valor;
+  struct no *proximo;
+} No;
+
+typedef struct fila {
+  No *primeiro;
+  No *ultimo;
+  int tam;
+} Fila;
+
+void criarFila(Fila *fila) {
+  fila->primeiro = NULL;
+  fila->ultimo = NULL;
+  fila->tam = 0;
+}
+
+void inserir(Fila *fila, int num) {
+  No *aux, *novo = malloc(sizeof(No));
+  if (novo) {
+    novo->valor = num;
+    novo->proximo = NULL;
+    if (fila->primeiro == NULL) {
+      fila->primeiro = novo;
+      fila->ultimo = novo;
+    } else {
+      fila->ultimo->proximo = novo;
+      fila->ultimo = novo;
+    }
+    fila->tam++;
+  }
+}
+
+No *remover(Fila *fila) {
+  No *remover = NULL;
+
+  if (fila->primeiro) {
+    remover = fila->primeiro;
+    fila->primeiro = remover->proximo;
+    fila->tam--;
+  } else {
+    printf("\nFila vazia!");
+  }
+
+  return remover;
+}
+
+void imprimir(Fila *fila) {
+  No *aux = fila->primeiro;
+
+  printf("\n\t-----------Fila-----------\n");
+  while (aux) {
+    printf("%d ", aux->valor);
+    aux = aux->proximo;
+  }
+  printf("\n\t--------Fim da Fila-------\n");
+}
+
+int lerInteiro(char *mensagem) {
+  int valorLido;
+  printf("%s", mensagem);
+  while (scanf(" %d", &valorLido) != true) {
+    while (getchar() != '\n')
+      printf("%s", mensagem);
+  }
+  return valorLido;
+}
+
+#define SAIR 0
+#define INSERIR 1
+#define REMOVER 2
+#define IMPRIMIR 3
+
+int main(void) {
+
+  No *removido;
+  Fila fila;
+  criarFila(&fila);
+  int opcao;
+
+  do {
+    printf("\t0 - Sair\n\t1 - Inserir\n\t2 - Remover\n\t3 - Imprimir\n");
+    scanf("%i", &opcao);
+    switch (opcao) {
+    case INSERIR:
+      inserir(&fila, lerInteiro("Digite um valor: "));
+      break;
+    case REMOVER:
+      removido = remover(&fila);
+      if (removido)
+        break;
+    case IMPRIMIR:
+      imprimir(&fila);
+      break;
+    case SAIR:
+      break;
+    default:
+      printf("\nOpção inválida!\n");
+      break;
+    }
+  } while (opcao != SAIR);
+
+  return 0;
+}
