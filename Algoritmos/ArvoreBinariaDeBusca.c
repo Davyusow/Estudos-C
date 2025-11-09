9145758703ff56024c8d3eaa226ca16117a9e318
Davyusow <dayvsonf27@gmail.com>
2025-11-08 22:48:41 -0300

    Atualizações nos algoritmos
    - Arvore binária de busca
    - Ajustes inicias em TabelaHash

--- Algoritmos/ArvoreBinariaDeBusca.c
+++ Algoritmos/ArvoreBinariaDeBusca.c
@@ -1,176 +1,207 @@
+#include "stdbool.h"
 #include "stdio.h"
 #include "stdlib.h"
 
-//constantes do menu
+// constantes do menu
 #define VER 1
 #define INSERIR 2
 #define BUSCAR 3
 #define EXCLUIR 4
 #define SAIR 0
-//constantes lógicos
-#define TRUE 1
-#define FALSE 0
 
-typedef struct No{
+typedef struct No {
   int dado;
+  struct No *pai;
   struct No *esquerda;
   struct No *direita;
-}No;
+} No;
 
-typedef struct{
+typedef struct {
   No *raiz;
-}Arvore;
+  int niveis;
+} Arvore;
 
 void imprimir(No *no) {
-    if (no != NULL) {
-        imprimir(no->esquerda);
-        printf("%i ", no->dado);
-        imprimir(no->direita);
-    }
+  if (no != NULL) {
+    imprimir(no->esquerda);
+    printf("%i ", no->dado);
+    imprimir(no->direita);
+  }
 }
 
-No *busca(Arvore *arvore ,int chave){
-  No *temp = arvore->raiz;
+No *busca(No *no, int chave) { // percurso em ordem
+  No *atual = no;
 
-  while(temp != NULL){
-    if(temp->dado == chave){
-      return temp;
-    }
-    else if(temp->dado > chave){
-      temp = temp->esquerda;
-    }else if(temp->dado < chave){
-      temp = temp->direita;
+  while (atual != NULL && atual->dado != chave) {
+    if (chave < atual->dado) {
+      atual = atual->esquerda;
+    } else {
+      atual = atual->direita;
     }
   }
-  return NULL;
+
+  return atual;
 }
 
 void inserir(Arvore *arvore, int chave) {
-    No *novo = (No *)calloc(1, sizeof(No));
-    novo->dado = chave;
-    novo->esquerda = NULL;
-    novo->direita = NULL;
+  No *novo = (No *)calloc(1, sizeof(No));
+  novo->dado = chave;
+  novo->esquerda = NULL;
+  novo->direita = NULL;
+  novo->pai = NULL;
 
-    if (arvore->raiz == NULL) {
-        arvore->raiz = novo;
-        return;
-    }
+  if (arvore->raiz == NULL) {
+    arvore->raiz = novo;
+    return;
+  }
 
-    No *atual = arvore->raiz;
-    No *pai = NULL;
+  No *atual = arvore->raiz;
+  No *pai = NULL;
 
-    while (atual != NULL) {
-        pai = atual;
-        if (chave < atual->dado) {
-            atual = atual->esquerda;
-        } else if (chave > atual->dado) {
-            atual = atual->direita;
-        } else {
-            free(novo);
-            return;
-        }
-    }
-
-    if (chave < pai->dado) {
-        pai->esquerda = novo;
+  while (atual != NULL) {
+    pai = atual;
+    if (chave <= atual->dado) {
+      atual = atual->esquerda;
     } else {
-        pai->direita = novo;
+      atual = atual->direita;
     }
+  }
+
+  novo->pai = pai;
+
+  if (chave <= pai->dado) {
+    pai->esquerda = novo;
+  } else {
+    pai->direita = novo;
+  }
 }
 
-No* encontrarMinimo(No* no) {
-    while (no->esquerda != NULL) {
-        no = no->esquerda;
+No *encontrarMinimo(No *no) {
+  while (no->esquerda != NULL) {
+    no = no->esquerda;
+  }
+
+  return no;
+}
+
+No *sucessor(No *no) {
+  if (no->direita != NULL) {
+    return encontrarMinimo(no->direita);
+  } else {
+    No *pai = no->pai;
+    while (pai != NULL && no == pai->direita) {
+      no = pai;
+      pai = pai->pai;
     }
-    return no;
+    return pai;
+  }
 }
 
 int excluir(Arvore *arvore, int chave) {
-    if (arvore->raiz == NULL) {
-        return FALSE;
+  if (arvore->raiz == NULL)
+    return false;
+
+  No *atual = arvore->raiz;
+  No *pai = NULL;
+
+  while (atual != NULL && atual->dado != chave) {
+    pai = atual;
+    if (chave < atual->dado)
+      atual = atual->esquerda;
+    else
+      atual = atual->direita;
+  }
+  if (atual == NULL)
+    return false;
+
+  // Caso 1: Nó sem filhos
+  if (atual->esquerda == NULL && atual->direita == NULL) {
+    if (pai == NULL)
+      arvore->raiz = NULL;
+    else if (pai->esquerda == atual)
+      pai->esquerda = NULL;
+    else
+      pai->direita = NULL;
+
+    free(atual);
+  }
+  // Caso 2: Nó com apenas um filho
+  else if (atual->esquerda == NULL || atual->direita == NULL) {
+    No *filho = (atual->esquerda != NULL) ? atual->esquerda : atual->direita;
+
+    if (filho != NULL) {
+      filho->pai = pai;
     }
 
-    No* atual = arvore->raiz;
-    No* pai = NULL;
-    
-    while (atual != NULL && atual->dado != chave) {
-       pai = atual;
-       if (chave < atual->dado) {
-           atual = atual->esquerda;
-       } else {
-           atual = atual->direita;
-       }
-    }
-    if (atual == NULL) {
-       return FALSE;
-    }
+    if (pai == NULL)
+      arvore->raiz = filho;
+    else if (pai->esquerda == atual)
+      pai->esquerda = filho;
+    else
+      pai->direita = filho;
 
-    // Caso 1: Nó sem filhos
-    if (atual->esquerda == NULL && atual->direita == NULL) {
-        if (pai == NULL) {
-            arvore->raiz = NULL;
-        } else if (pai->esquerda == atual) {
-            pai->esquerda = NULL;
+    free(atual);
+  }
+  // Caso 3: Nó com dois filhos
+  else {
+    No *suc = sucessor(atual);
+
+    if (suc != NULL) {
+      int valorSucessor = suc->dado;
+      No *paiSuc = suc->pai;
+      No *filhoSuc = suc->direita;
+
+      if (filhoSuc != NULL) {
+        filhoSuc->pai = paiSuc;
+      }
+
+      if (paiSuc != NULL) {
+        if (paiSuc->esquerda == suc) {
+          paiSuc->esquerda = filhoSuc;
         } else {
-            pai->direita = NULL;
+          paiSuc->direita = filhoSuc;
         }
-        free(atual);
-    }
-    // Caso 2: Nó com apenas um filho
-    else if (atual->esquerda == NULL || atual->direita == NULL) {
-        No* filho = (atual->esquerda != NULL) ? atual->esquerda : atual->direita;
-        if (pai == NULL) {
-            arvore->raiz = filho;
-        } else if (pai->esquerda == atual) {
-            pai->esquerda = filho;
-        } else {
-            pai->direita = filho;
-        }
-        free(atual);
-    }
-    // Caso 3: Nó com dois filhos
-    else {
-        No* sucessor = encontrarMinimo(atual->direita);
-        int valorSucessor = sucessor->dado;
-        excluir(arvore, valorSucessor);
-        atual->dado = valorSucessor;
-    }
+      }
 
-    return TRUE;
+      atual->dado = valorSucessor;
+      free(suc);
+    }
+  }
+
+  return true;
 }
 
 int lerInteiro(char *mensagem) {
   int valorLido;
   printf("%s", mensagem);
-  while (scanf(" %d", &valorLido) != TRUE) {
-    while (getchar() != '\n');
+  while (scanf(" %d", &valorLido) != true) {
+    while (getchar() != '\n')
+      ;
     printf("%s", mensagem);
   }
   return valorLido;
 }
 
-int main(void){
+int main(void) {
 
-Arvore arvore;
-int opcao,tamanho;
+  Arvore arvore;
+  int opcao, tamanho;
+  arvore.raiz = NULL;
 
+  do {
+    int tempChave = -1;
+    printf("---------------------------------------"
+           "\n\tÁrvore Binária de Busca\n"
+           "---------------------------------------\n");
+    printf(" Digite um número para escolher a opção:  \n"
+           " 1 ⟶ Para Imprimir a árvore\n"
+           " 2 ⟶ Para inserir um valor\n"
+           " 3 ⟶ Para buscar um valor\n"
+           " 4 ⟶ Para excluir um valor\n"
+           " 0 ⟶ Para sair\n");
+    opcao = lerInteiro("Opção: ");
 
-arvore.raiz = NULL;
-
-do{
-  int tempChave = -1;
-printf("---------------------------------------"
-              "\n\tÁrvore Binária de Busca\n"
-              "---------------------------------------\n");
-  printf(" Digite um número para escolher a opção:  \n"
-        " 1 ⟶ Para Imprimir a árvore\n"
-        " 2 ⟶ Para inserir um valor\n"
-        " 3 ⟶ Para buscar um valor\n"
-        " 4 ⟶ Para excluir um valor\n"
-        " 0 ⟶ Para sair\n");
-        opcao = lerInteiro("Opção: ");
-    
-  switch(opcao){
+    switch (opcao) {
     default:
       system("clear");
       printf("Opção inválida!\n");
@@ -185,41 +216,45 @@ printf("---------------------------------------"
     case INSERIR:
       system("clear");
       printf("Inserindo...\n");
-      tempChave = lerInteiro("Insira aqui o valor da chave que deseja inserir: ");
-      inserir( &arvore,  tempChave);
+      tempChave =
+          lerInteiro("Insira aqui o valor da chave que deseja inserir: ");
+      inserir(&arvore, tempChave);
       break;
     case BUSCAR:
       system("clear");
       printf("Buscando...\n");
       tempChave = lerInteiro("Digite o valor que deseja buscar: ");
-      No *tempBusca = busca(&arvore, tempChave);
-      if(tempBusca != NULL){
-        printf("O valor %i foi encontrado.\n",tempChave);
-        if(tempBusca->esquerda != NULL)
-          printf("O valor à esquerda de %i é %i\n",tempChave, tempBusca->esquerda->dado);
-        if(tempBusca->direita != NULL)
-        printf("O valor à direita de %i é %i\n",tempChave, tempBusca->direita->dado);
-      }
-      else
-        printf("%i não esta presente no vetor.\n",tempChave);
+      No *tempBusca = busca(arvore.raiz, tempChave);
+      if (tempBusca != NULL) {
+        printf("O valor %i foi encontrado.\n", tempChave);
+        if (tempBusca->pai != NULL)
+          printf("O pai de %i é %i\n", tempChave, tempBusca->pai->dado);
+        if (tempBusca->esquerda != NULL)
+          printf("O valor à esquerda de %i é %i\n", tempChave,
+                 tempBusca->esquerda->dado);
+        if (tempBusca->direita != NULL)
+          printf("O valor à direita de %i é %i\n", tempChave,
+                 tempBusca->direita->dado);
+      } else
+        printf("%i não esta presente no vetor.\n", tempChave);
       break;
     case EXCLUIR:
       system("clear");
       printf("Excluindo...\n");
       tempChave = lerInteiro("Digite o valor que deseja remover da lista: ");
-      tempBusca = busca(&arvore, tempChave);
-      if(excluir(&arvore, tempChave))
-        printf("%i removido com sucesso!\n",tempChave);
+      tempBusca = busca(arvore.raiz, tempChave);
+      if (excluir(&arvore, tempChave))
+        printf("%i removido com sucesso!\n", tempChave);
       else
-        printf("O valor %i não foi encontrado no vetor.\n",tempChave);
+        printf("O valor %i não foi encontrado no vetor.\n", tempChave);
       break;
     case SAIR:
       system("clear");
       printf("Saindo...\n");
       break;
-  }
+    }
 
-}while(opcao != SAIR);
+  } while (opcao != SAIR);
   printf("Até logo!");
   free(arvore.raiz);
   return 0;
--- Algoritmos/TabelaHash.c
+++ Algoritmos/TabelaHash.c
@@ -1,10 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "stdbool.h"
 
 // contantes lógicos
-#define TRUE 1
-#define FALSE 0
-#define NAO_ENCONTRADO -1
 #define NIL -1
 
 // constantes da tabela Hash
@@ -20,26 +18,26 @@
 #define SAIR 0
 
 // seleção da lista
-#define SEQUENCIAL 0
-#define ENCADEADA 1
+#define OPEN 0
+#define CLOSED 1
 
 typedef struct {
   int chave;
   int estado;
-} Registro; // open addres
+} NoOpen; // open addres
 
 typedef struct No {
   int chave;
-  struct No *prox;
+  struct No *proximo;
   struct No *anterior;
-} No; // closed addres
+} NoClosed; // closed addres
 
 typedef struct {
   int tamanho;
   int ocupados;
-  Registro *dados;
-  No **lista;
-  int usandoLista;
+  NoOpen *dados;
+  NoClosed **lista;
+  int adress;
 } TabelaHash;
 
 int funcaoHash(TabelaHash *tabela, int chave) {
@@ -48,8 +46,8 @@ int funcaoHash(TabelaHash *tabela, int chave) {
 
 void iniciaTabela(TabelaHash *tabela, int tamanho) {
   tabela->tamanho = tamanho;
-  tabela->dados = (Registro *)malloc(sizeof(Registro) * tamanho);
-  tabela->usandoLista = SEQUENCIAL;
+  tabela->dados = (NoOpen *)malloc(sizeof(NoOpen) * tamanho);
+  tabela->adress = OPEN;
 
   for (int i = 0; i < tamanho; i++) {
     tabela->dados[i].chave = NIL;
@@ -60,34 +58,34 @@ void iniciaTabela(TabelaHash *tabela, int tamanho) {
 }
 
 int buscaTabela(TabelaHash *tabela, int chave) {
-  if (tabela->usandoLista == SEQUENCIAL) {
+  if (tabela->adress == OPEN) {
     int indice = funcaoHash(tabela, chave);
     int contador = 0;
     while (contador < tabela->tamanho) {
       if (tabela->dados[indice].estado == VAZIO)
-        return NAO_ENCONTRADO;
+        return NIL;
       if (tabela->dados[indice].estado == OCUPADO &&
           tabela->dados[indice].chave == chave)
         return indice;
       indice = (indice + 1) % tabela->tamanho; // sondagem linear
       contador++;
     }
-    return NAO_ENCONTRADO;
-  } else if (tabela->usandoLista == ENCADEADA) { // closed addres com lista ligada
+    return NIL;
+  } else if (tabela->adress == CLOSED) { // closed addres com lista ligada
     int indice = funcaoHash(tabela, chave);
-    No *atual = tabela->lista[indice];
+    NoClosed *atual = tabela->lista[indice];
     while (atual != NULL) {
       if (atual->chave == chave)
         return indice;
-      atual = atual->prox;
+      atual = atual->proximo;
     }
   }
-  return NAO_ENCONTRADO;
+  return NIL;
 }
 
 void trocaLista(TabelaHash *tabela) {
   printf("Mudando para closed addressing...\n");
-  tabela->lista = (No **)malloc(sizeof(No *) * tabela->tamanho);
+  tabela->lista = (NoClosed **)malloc(sizeof(NoClosed *) * tabela->tamanho);
   for (int i = 0; i < tabela->tamanho; i++) {
     tabela->lista[i] = NULL;
   }
@@ -96,9 +94,9 @@ void trocaLista(TabelaHash *tabela) {
       int chave = tabela->dados[i].chave;
       int indice = funcaoHash(tabela, chave);
 
-      No *novo = (No *)malloc(sizeof(No));
+      NoClosed *novo = (NoClosed *)malloc(sizeof(NoClosed));
       novo->chave = chave;
-      novo->prox = tabela->lista[indice];
+      novo->proximo = tabela->lista[indice];
       novo->anterior = NULL;
       if (tabela->lista[indice] != NULL)
         tabela->lista[indice]->anterior = novo;
@@ -108,16 +106,16 @@ void trocaLista(TabelaHash *tabela) {
   }
   free(tabela->dados);
   tabela->dados = NULL;
-  tabela->usandoLista = ENCADEADA;
+  tabela->adress = CLOSED;
 }
 
 int inserirTabela(TabelaHash *tabela, int chave) {
-  if (buscaTabela(tabela, chave) != NAO_ENCONTRADO) {
+  if (buscaTabela(tabela, chave) != NIL) {
     printf("Elemento já inserido na tabela!\n");
-    return FALSE;
+    return false;
   }
 
-  if (tabela->usandoLista == SEQUENCIAL) {
+  if (tabela->adress == OPEN) {
     int indice = funcaoHash(tabela, chave);
     int contador = 0;
 
@@ -126,7 +124,7 @@ int inserirTabela(TabelaHash *tabela, int chave) {
         tabela->dados[indice].chave = chave;
         tabela->dados[indice].estado = OCUPADO;
         tabela->ocupados++;
-        return TRUE;
+        return true;
       }
       indice = (indice + 1) % tabela->tamanho;
       contador++;
@@ -134,13 +132,13 @@ int inserirTabela(TabelaHash *tabela, int chave) {
     printf("Tabela Hash cheia!\n"); // usar closed address hashing
     trocaLista(tabela);
     inserirTabela(tabela,chave);
-    return FALSE;
-  } else if (tabela->usandoLista == ENCADEADA) {
+    return false;
+  } else if (tabela->adress == CLOSED) {
     int indice = funcaoHash(tabela, chave);
 
-    No *novo = (No *)malloc(sizeof(No));
+    NoClosed *novo = (NoClosed *)malloc(sizeof(NoClosed));
     novo->chave = chave;
-    novo->prox = tabela->lista[indice];
+    novo->proximo = tabela->lista[indice];
     novo->anterior = NULL;
 
     if (tabela->lista[indice] != NULL)
@@ -148,52 +146,52 @@ int inserirTabela(TabelaHash *tabela, int chave) {
 
     tabela->lista[indice] = novo;
     tabela->ocupados++;
-    return TRUE;
+    return true;
   }
-  return FALSE;
+  return false;
 }
 
 int excluirTabela(TabelaHash *tabela, int chave) {
-  if (tabela->usandoLista == SEQUENCIAL) {
+  if (tabela->adress == OPEN) {
     int indice = buscaTabela(tabela, chave);
-    if (indice == NAO_ENCONTRADO) {
-      return FALSE;
+    if (indice == NIL) {
+      return false;
     }
     tabela->dados[indice].estado = REMOVIDO;
     tabela->ocupados--;
-    return TRUE;
-  } else if (tabela->usandoLista == ENCADEADA) {
+    return true;
+  } else if (tabela->adress == CLOSED) {
     int indice = funcaoHash(tabela, chave);
-    No *atual = tabela->lista[indice];
-    No *anterior = NULL;
+    NoClosed *atual = tabela->lista[indice];
+    NoClosed *anterior = NULL;
 
     while (atual != NULL) {
       if (atual->chave == chave) {
         if (atual->anterior == NULL) {
-          tabela->lista[indice] = atual->prox;
-          if (atual->prox != NULL) {
-            atual->prox->anterior = NULL;
+          tabela->lista[indice] = atual->proximo;
+          if (atual->proximo != NULL) {
+            atual->proximo->anterior = NULL;
           }
         } else {
-          atual->anterior->prox = atual->prox;
-          if (atual->prox != NULL) {
-            atual->prox->anterior = atual->anterior;
+          atual->anterior->proximo = atual->proximo;
+          if (atual->proximo != NULL) {
+            atual->proximo->anterior = atual->anterior;
           }
         }
         free(atual);
-        return TRUE;
+        return true;
       }
-      atual = atual->prox;
+      atual = atual->proximo;
     }
-    return FALSE;
+    return false;
   }
-  return FALSE;
+  return false;
 }
 
 void imprimeTabela(TabelaHash *tabela) {
   printf("[ ");
 
-  if (tabela->usandoLista == SEQUENCIAL) {
+  if (tabela->adress == OPEN) {
     for (int i = 0; i < tabela->tamanho; i++) {
       if (tabela->dados[i].estado == REMOVIDO ||
           tabela->dados[i].estado == VAZIO)
@@ -201,12 +199,12 @@ void imprimeTabela(TabelaHash *tabela) {
       else
         printf("%i ", tabela->dados[i].chave);
     }
-  } else if (tabela->usandoLista == ENCADEADA) {
+  } else if (tabela->adress == CLOSED) {
     for (int i = 0; i < tabela->tamanho; i++) {
-      No *atual = tabela->lista[i];
+      NoClosed *atual = tabela->lista[i];
       while (atual != NULL) {
         printf("%i ", atual->chave);
-        atual = atual->prox;
+        atual = atual->proximo;
       }
     }
   }
@@ -216,7 +214,7 @@ void imprimeTabela(TabelaHash *tabela) {
 int lerInteiro(char *mensagem) {
   int valorLido;
   printf("%s", mensagem);
-  while (scanf(" %d", &valorLido) != TRUE) {
+  while (scanf(" %d", &valorLido) != true) {
     while (getchar() != '\n')
       printf("%s", mensagem);
   }
@@ -238,12 +236,12 @@ int main(void) {
 
   do {
     int tempChave = NIL;
-    if(tabela.usandoLista == SEQUENCIAL){
+    if(tabela.adress == OPEN){
       printf("---------------------------------------"
            "\nLista Linear Sequencial com Tabela Hash\n"
            "---------------------------------------\n");}
 
-    else if(tabela.usandoLista == ENCADEADA){
+    else if(tabela.adress == CLOSED){
       printf("------------------------------------------"
            "\nLista Duplamente Encadeada com Tabela Hash\n"
            "------------------------------------------\n");}
@@ -278,7 +276,7 @@ int main(void) {
       printf("Buscando...\n");
       tempChave = lerInteiro("Digite o valor que deseja buscar: ");
       int tempBusca = buscaTabela(&tabela, tempChave);
-      if (tempBusca != NAO_ENCONTRADO)
+      if (tempBusca != NIL)
         printf("O valor %i foi encontrado no índice %i.\n", tempChave, tempBusca);
       else
         printf("%i não esta presente no vetor.\n", tempChave);
@@ -287,7 +285,7 @@ int main(void) {
       system("clear");
       printf("Excluindo...\n");
       tempChave = lerInteiro("Digite o valor que deseja remover da tabela: ");
-      if (excluirTabela(&tabela, tempChave) == TRUE)
+      if (excluirTabela(&tabela, tempChave) == true)
         printf("Valor removido com sucesso!\n");
       else
         printf("Valor não existe na tabela!\n");
