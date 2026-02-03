bb0475ac068334655c0ecf1a3844254c36444ea0
Davyusow <dayvsonf27@gmail.com>
2026-02-03 12:10:46 -0300

    Ajustes e rascunhos rápidos

--- testes/aesthetic_bubble_sort.c
+++ testes/aesthetic_bubble_sort.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define TAM 10
 
@@ -16,14 +15,20 @@ int main(void){
 
   // Lista lista = malloc(sizeof(*Objeto)*TAM)
   printf("Tudo certo até aqui?\n");
-  
-  
+
+  int dados[10] = {2,5,2,5,7,43,7,9,3,1};
+  int resultado[10];
   for (int i = 0;i < 10;i++) {
     for (int j = 0;j < 10;j++) {
+      if (dados[i] > dados[i+1]) {
+        int temp = dados[i];
+        dados[i+1] =
+        dados[i] = dados[i+1];
+      }
       printf("Valor: %d\r",i);
     }
   }
-  
+
 
   return 0;
 }
---
+++ testes/exemplo.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+int main(void) {
+  printf("Beltrano\n");
+  system("echo \"Beltrano\" >> arquivo.txt"); // Usando um comando shell só de meme
+  return 0;
+}
